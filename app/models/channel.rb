4befe187ab6aadc5e5877e0846927897d178488b
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-14 11:20:47 -0300

    Created team_user model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,4 +3,5 @@ class Channel < ApplicationRecord
   belongs_to :team
   belongs_to :user
   validates_presence_of :slug, :team, :user
+  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
---
+++ app/models/team_user.rb
@@ -0,0 +1,5 @@
+class TeamUser < ApplicationRecord
+  belongs_to :team
+  belongs_to :user
+  validates_presence_of :team, :user
+end
