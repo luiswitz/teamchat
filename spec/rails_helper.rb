47cdea6b12daf54f185d060dee9c501ac7623d1b
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-27 10:12:12 -0300

    Enable users to leave teams

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
+
+include Warden::Test::Helpers
 
 ActiveRecord::Migration.maintain_test_schema!
 
